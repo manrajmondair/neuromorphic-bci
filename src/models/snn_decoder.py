83866a45a7bcb9fb7b69efd36d4a85e0a00e0aae
alexsoto1224 <124871403+alexsoto1224@users.noreply.github.com>
2026-05-25 14:48:02 -0700

    Update snn_decoder.py

--- src/models/snn_decoder.py
+++ src/models/snn_decoder.py
@@ -39,8 +39,20 @@ class SparseLatencySNN:
         val_idx: np.ndarray,
     ) -> "SparseLatencySNN":
 
+        num_bins = len(event_times)
         rng = np.random.default_rng(self.seed)
         self.W = rng.normal(loc=0, scale=1/np.sqrt(self.num_neurons), size=(self.hidden_dim, self.num_neurons))
+        Z = np.zeros((num_bins, self.hidden_dim), np.float32)
+        
+        for b in range(num_bins):
+            u = np.zeros(self.hidden_dim, np.float32)
+            spikes = event_neurons[b]
+            for neuron in spikes:
+                u =  self.beta * u + W[:, neuron]
+                u[u >= self.threshold] -= self.threshold
+            Z[b] = u
+
+        Z_train
         
         raise NotImplementedError
 
